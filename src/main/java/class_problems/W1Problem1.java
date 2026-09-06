96c72786c74ee65a0d40796fcd5dd0bae6df0704
Tarun Suresh <tarunsuresh07@gmail.com>
2026-09-06 22:48:29 +0530

    Week 2 Completed

---
+++ src/main/java/class_problems/W1Problem1.java
@@ -0,0 +1,22 @@
+public class W1Problem1 {
+    public static void countVowelsAndConsonants(String text) {
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char character = Character.toLowerCase(text.charAt(i));
+
+            if (character == 'a' || character == 'e' || character == 'i'
+                    || character == 'o' || character == 'u')
+                vowels++;
+            else if (character != ' ')
+                consonants++;
+        }
+
+        System.out.println("Vowels: " + vowels + " | Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+        countVowelsAndConsonants("Java Programming");
+    }
+}
